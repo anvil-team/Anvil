1a00d349caa6dd231dd209866cbb6951c1e5ad74
zsirfs <zhenglfsir@gmail.com>
2018-12-25 22:03:24 +0800

    修改文件结构

--- anvil-web/src/components/AuthRoute.jsx
+++ anvil-web/src/components/AuthRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import isDev from 'src/utils/isDev'
 import { Spin, Icon } from 'antd'
 
--- anvil-web/src/routes/router.js
+++ anvil-web/src/routes/router.js
@@ -1,4 +1,4 @@
-import LazyLoad from 'src/components/LazyLoad'
+import LazyLoad from 'src/components/LazyLoad';
 
 export const routes = [
   {
@@ -8,18 +8,18 @@ export const routes = [
       {
         path: '/',
         title: '面板',
-        component: LazyLoad(() => import('./dashboard')),
+        component: LazyLoad(() => import('./dashboard/Dashboard')),
       },
     ],
   },
   {
     path: '/login',
     title: '登录',
-    component: LazyLoad(() => import('./login')),
+    component: LazyLoad(() => import('./login/Login')),
   },
   {
     path: '/',
     title: 'Anvil',
     redirect: '/login',
   },
-]
+];
