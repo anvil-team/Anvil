e895b157fb02e1a2ada8db2de1690a0e0da1fc86
zsirfs <zhenglfsir@gmail.com>
2019-02-19 09:18:59 +0800

    commit the gzip compression and use the after_script of job lifecycle

--- anvil-web/scripts/deploy.js
+++ anvil-web/scripts/deploy.js
@@ -1,11 +1,7 @@
 const http = require('http');
 
-console.log('---------------------------------------');
-
 const req = http.request('http://39.105.38.144:3333/restart', {
   method: 'GET',
 });
 
 req.end();
-
-console.log('------------------------------------');
